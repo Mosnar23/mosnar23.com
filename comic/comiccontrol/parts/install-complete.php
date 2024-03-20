3ff486c3e998b4a64f00d9c576a397c0224a6ae1
Mosnar23 <mosnar23@icloud.com>
2024-03-20 16:23:31 -0700

    ByeBlocker

--- comic/comiccontrol/parts/install-complete.php
+++
@@ -1,5 +0,0 @@
-<?php include('includes/install-header.php');  ?>
-
-<?=$ilang['complete']?>
-
-<?php include('includes/install-footer.php'); ?>
--- comic/templates/basic/page.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('templates/basic/includes/header.php'); ?>
-
-<?php $ccpage->module->display(); ?>
-
-<?php include('templates/basic/includes/footer.php'); ?>
